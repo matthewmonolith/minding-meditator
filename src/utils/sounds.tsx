3e1b5e43763cdd4b61ef9f8d54d4871a50d151c7
matthewmonolith <matthew.page2016@outlook.com>
2024-11-12 11:55:23 +0000

    moved sounds to public for deployment

--- src/utils/sounds.tsx
+++ src/utils/sounds.tsx
@@ -1,11 +1,11 @@
 export const reminderSounds = [
-    {name: "Chime", soundPath: "src/assets/Sounds/chime.mp3" },
-    {name:"Deep Breath", soundPath:"src/assets/Sounds/deepbreath.mp3"},
-    {name:"Ding", soundPath:"src/assets/Sounds/ding.mp3"}
-]
+    { name: "Chime", soundPath: "/sounds/chime.mp3" },
+    { name: "Deep Breath", soundPath: "/sounds/deepbreath.mp3" },
+    { name: "Ding", soundPath: "/sounds/ding.mp3" }
+];
 
 export const meditationSounds = [
-    {name:"Wind Chimes", soundPath: "src/assets/Sounds/windchimes.mp3"},
-    {name:"Piano", soundPath: "src/assets/Sounds/piano.mp3"},
-    {name:"Melody", soundPath: "src/assets/Sounds/melody.mp3"},
-]
+    { name: "Wind Chimes", soundPath: "/sounds/windchimes.mp3" },
+    { name: "Piano", soundPath: "/sounds/piano.mp3" },
+    { name: "Melody", soundPath: "/sounds/melody.mp3" }
+];
