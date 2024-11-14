717d227aa59328cae990aaeea8b0cab533eb2448
matthewmonolith <matthew.page2016@outlook.com>
2024-11-14 15:55:05 +0000

    removed test times

--- src/utils/times.tsx
+++ src/utils/times.tsx
@@ -1,6 +1,6 @@
-export const mainTimes = ["0:05","05:00", "10:00", "15:00", "20:00", "30:00"];
+export const mainTimes = ["05:00", "10:00", "15:00", "20:00", "30:00"];
 
-export const reminderTimes = ["0:05","00:30", "01:00", "01:30", "02:00"];
+export const reminderTimes = ["00:30", "01:00", "01:30", "02:00"];
 
 export const newTimeStamp = (timeStamp: string, forReset: boolean = false) => {
   const [minutes, seconds] = timeStamp.split(":").map(Number);
